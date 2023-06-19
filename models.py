c17d115f1d73d8aa5091356f3066fec02514ec74
DESKTOP-5F8KC74\ap0 <mimieszczak@gmail.com>
2023-06-19 23:30:24 +0200

    dodanie modeli

---
+++ models.py
@@ -0,0 +1,25 @@
+import uuid
+from peewee import *
+import datetime
+
+class BaseModel(Model):
+    id = UUIDField(primary_key=True, default=uuid.uuid4)
+    created_at = DateField(default=datetime.datetime.now)
+    
+    class Meta:
+        database = Config.DATABASE
+        abstract = True
+
+class Praca(BaseModel):
+    nazwa = CharField()
+    opis = CharField()
+    deadline = DateField()
+    taken = BooleanField(default=False)
+
+class Zgloszenie(BaseModel):
+    email = CharField()
+    wiadomosc = CharField()
+    nr_telefonu = CharField(null=True)
+    praca = ForeignKeyField(Praca, backref='zgloszenia')
+
+Config.DATABASE.create_tables([Praca, Zgloszenie])
